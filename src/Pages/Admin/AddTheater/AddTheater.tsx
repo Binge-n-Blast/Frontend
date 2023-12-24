c18cd807a613c1a645037ffbd53206c9f6a64504
Mohd Zaid <mohdzaidali75@gmail.com>
2023-12-25 00:49:15 +0530

    added pages in admin section

--- src/Pages/Admin/AddTheater/AddTheater.tsx
+++ src/Pages/Admin/AddTheater/AddTheater.tsx
@@ -1,10 +1,13 @@
-import Navbar from "../../../Components/Admin/Navbar/Navbar";
 import "./AddTheater.scss";
+import Navbar from "../../../Components/Admin/Navbar/Navbar";
+
+// Images
+import store from "../../../Components/Admin/Sidebar/Assets/store.png"
 
 const AddTheater = () => {
   return (
       <>
-      <Navbar title="Add Theater"/>
+      <Navbar title="Add Theater" image={store}/>
       <section className="add-theaters"></section>
       </>
   );
