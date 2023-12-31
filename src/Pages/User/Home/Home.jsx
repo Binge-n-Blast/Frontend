e36be30bd23b2b04cac94c28fbf21e45b51b2bb4
Mohd Zaid <mohdzaidali75@gmail.com>
2023-12-31 15:05:39 +0530

    made home page resposive

--- src/Pages/User/Home/Home.jsx
+++ src/Pages/User/Home/Home.jsx
@@ -9,7 +9,7 @@ const Home = () => {
   return (
     <section className="home">
       <Hero />
-      <Celebrations />
+      {/* <Celebrations /> */}
       <CommonSections />
       <BookEvent /> 
       <Contact />
