3205656a7c046f402dc0daf941997b5c36efc67b
Mohd Zaid <mohdzaidali75@gmail.com>
2024-01-16 16:16:57 +0530

    changed checkout

--- src/Pages/User/Theater/Components/Checkout/Checkout.jsx
+++ src/Pages/User/Theater/Components/Checkout/Checkout.jsx
@@ -74,97 +74,176 @@ const Checkout = ({ info }) => {
     dispatch(setModalOpen());
   };
 
-
   return (
     <>
       <CheckoutModal />
-      <section className="checkout">
-        {isCheckoutActive ? (
-          <>
-            <div className="top" onClick={handleCheckoutActive}>
-              <h2>Booking Summary</h2>
-              <FaMinus
-                style={{ color: "white", cursor: "pointer" }}
-                size={16}
-              />
-            </div>
+      {window.innerWidth > 1100 ? (
+        <section className="desktop-checkout">
+          <div className="content">
+          <div className="top">
+            <h2>Booking Summary</h2>
+          </div>
 
-            <div className="main">
-              <div className="data">
-                <h3>{theater.theaterName}</h3>
-                <p>₹ {price}</p>
-              </div>
-              <div className="date">
-                <p>
-                  Date:<span> {date}</span>
-                </p>
-                <p>
-                  Slot: <span> {slot && slot.timing}</span>
-                </p>
-                <p>
-                  People: <span> {person}</span>
-                </p>
-              </div>
-              <hr />
+          <div className="main">
+            <div className="data">
+              <h3>{theater.theaterName}</h3>
+              <p>₹ {price}</p>
             </div>
-
-            <div className="rest">
-              <h3>Event Decoration</h3>
-              <div className="data">
-                {info && info.decoration ? (
-                  <div className="data">
-                    <p>{info.decoration.itemsName}</p>
-                    <p>₹ {info.decoration.price}</p>
-                  </div>
-                ) : (
-                  ""
-                )}
-              </div>
-              <hr />
+            <div className="date">
+              <p>
+                Date:<span> {date}</span>
+              </p>
+              <p>
+                Slot: <span> {slot && slot.timing}</span>
+              </p>
+              <p>
+                People: <span> {person}</span>
+              </p>
             </div>
+            <hr />
+          </div>
 
-            <div className="rest">
-              <h3>Cakes</h3>
-              <div className="data">
-                {info && info.cake ? (
-                  <div className="data">
-                    <p>{info.cake.itemsName}</p>
-                    <p>₹ {info.cake.price}</p>
-                  </div>
-                ) : (
-                  ""
-                )}
-              </div>
-              <hr />
-            </div>
-
-            <div className="rest">
-              <h3>Add On’s</h3>
-              {info && info.addOns ? (
+          <div className="rest">
+            <h3>Event Decoration</h3>
+            <div className="data">
+              {info && info.decoration ? (
                 <div className="data">
-                  <p>{info.addOns.itemsName}</p>
-                  <p>₹ {info.addOns.price}</p>
+                  <p>{info.decoration.itemsName}</p>
+                  <p>₹ {info.decoration.price}</p>
                 </div>
               ) : (
                 ""
               )}
-              <hr />
             </div>
-
-            <div className="total">
-              <h2>Grand Total</h2>
-              <h2>₹{grandTotal}</h2>
-            </div>
-
-            <button onClick={handleSubmit}>Proceed to checkout</button>
-          </>
-        ) : (
-          <div className="top" onClick={handleCheckoutActive}>
-            <h2>Booking Summary</h2>
-            <FaPlus style={{ color: "white", cursor: "pointer" }} size={16} />
+            <hr />
           </div>
-        )}
-      </section>
+
+          <div className="rest">
+            <h3>Cakes</h3>
+            <div className="data">
+              {info && info.cake ? (
+                <div className="data">
+                  <p>{info.cake.itemsName}</p>
+                  <p>₹ {info.cake.price}</p>
+                </div>
+              ) : (
+                ""
+              )}
+            </div>
+            <hr />
+          </div>
+
+          <div className="rest">
+            <h3>Add On’s</h3>
+            {info && info.addOns ? (
+              <div className="data">
+                <p>{info.addOns.itemsName}</p>
+                <p>₹ {info.addOns.price}</p>
+              </div>
+            ) : (
+              ""
+            )}
+            <hr />
+          </div>
+
+          <div className="total">
+            <h2>Grand Total</h2>
+            <h2>₹{grandTotal}</h2>
+          </div>
+
+          </div>
+          <button onClick={handleSubmit}>Proceed to checkout</button>
+        </section>
+      ) : (
+        <section className="mobile-checkout">
+          {isCheckoutActive ? (
+            <>
+              <div className="top" onClick={handleCheckoutActive}>
+                <h2>Booking Summary</h2>
+                <FaMinus
+                  style={{ color: "white", cursor: "pointer" }}
+                  size={16}
+                />
+              </div>
+
+              <div className="main">
+                <div className="data">
+                  <h3>{theater.theaterName}</h3>
+                  <p>₹ {price}</p>
+                </div>
+                <div className="date">
+                  <p>
+                    Date:<span> {date}</span>
+                  </p>
+                  <p>
+                    Slot: <span> {slot && slot.timing}</span>
+                  </p>
+                  <p>
+                    People: <span> {person}</span>
+                  </p>
+                </div>
+                <hr />
+              </div>
+
+              <div className="rest">
+                <h3>Event Decoration</h3>
+                <div className="data">
+                  {info && info.decoration ? (
+                    <div className="data">
+                      <p>{info.decoration.itemsName}</p>
+                      <p>₹ {info.decoration.price}</p>
+                    </div>
+                  ) : (
+                    ""
+                  )}
+                </div>
+                <hr />
+              </div>
+
+              <div className="rest">
+                <h3>Cakes</h3>
+                <div className="data">
+                  {info && info.cake ? (
+                    <div className="data">
+                      <p>{info.cake.itemsName}</p>
+                      <p>₹ {info.cake.price}</p>
+                    </div>
+                  ) : (
+                    ""
+                  )}
+                </div>
+                <hr />
+              </div>
+
+              <div className="rest">
+                <h3>Add On’s</h3>
+                {info && info.addOns ? (
+                  <div className="data">
+                    <p>{info.addOns.itemsName}</p>
+                    <p>₹ {info.addOns.price}</p>
+                  </div>
+                ) : (
+                  ""
+                )}
+                <hr />
+              </div>
+
+              <div className="total">
+                <h2>Grand Total</h2>
+                <h2>₹{grandTotal}</h2>
+              </div>
+
+              <button onClick={handleSubmit}>Proceed to checkout</button>
+            </>
+          ) : (
+            <div className="top" onClick={handleCheckoutActive}>
+              <h2>Booking Summary</h2>
+              <FaPlus style={{ color: "white", cursor: "pointer" }} size={16} />
+            </div>
+          )}
+        </section>
+      )}
+    
     </>
   );
 };
